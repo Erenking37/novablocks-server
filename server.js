b28a8d79bf3cfc4854d3bd5ffb6c43cea3f64e6c
Erenking37 <kazanedizeren@gmail.com>
2026-06-17 21:06:53 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -3,50 +3,41 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
-const PORT = process.env.PORT || 3000;
-
 app.use(express.static("public"));
 
 let players = {};
 
 io.on("connection",(socket)=>{
 
-socket.on("join",()=>{
+socket.on("join",(name)=>{
 
 players[socket.id] = {
 x:0,
 y:2,
-z:0,
-rotY:0
+z:0
 };
 
 socket.emit("init",players);
-socket.broadcast.emit("newPlayer",socket.id);
+socket.broadcast.emit("new",socket.id);
 
 });
 
 socket.on("move",(data)=>{
-
-if(!players[socket.id]) return;
-
 players[socket.id] = data;
-
-socket.broadcast.emit("update",{
-id:socket.id,
-data
+socket.broadcast.emit("update",{id:socket.id,data});
 });
 
+socket.on("chat",(msg)=>{
+io.emit("chat",{id:socket.id,msg});
 });
 
 socket.on("disconnect",()=>{
-
 delete players[socket.id];
 io.emit("remove",socket.id);
-
 });
 
 });
 
-http.listen(PORT,()=>{
-console.log("NovaBlocks v3.5 running:",PORT);
+http.listen(process.env.PORT || 3000,()=>{
+console.log("NovaBlocks v3.7 running");
 });
