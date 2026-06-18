c1339e334b31a716598a714d315377ddd3d8eeea
Erenking37 <kazanedizeren@gmail.com>
2026-06-18 09:11:41 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,43 +1,68 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
-app.use(express.static("public"));
+/* =========================
+   STATIC FILES (SENİN YAPIN)
+========================= */
+app.use(express.static(__dirname));
+
+/* MAIN PAGE */
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "roblox.html"));
+});
+
+/* =========================
+   MULTIPLAYER SYSTEM
+========================= */
 
 let players = {};
 
-io.on("connection",(socket)=>{
+io.on("connection", (socket) => {
 
-socket.on("join",(name)=>{
+    socket.on("join", () => {
 
-players[socket.id] = {
-x:0,
-y:2,
-z:0
-};
+        players[socket.id] = {
+            x: 0,
+            y: 2,
+            z: 0
+        };
 
-socket.emit("init",players);
-socket.broadcast.emit("new",socket.id);
+        socket.emit("init", players);
+        socket.broadcast.emit("new", socket.id);
+    });
+
+    socket.on("move", (data) => {
+        players[socket.id] = data;
+        socket.broadcast.emit("update", {
+            id: socket.id,
+            data
+        });
+    });
+
+    socket.on("chat", (msg) => {
+        io.emit("chat", {
+            id: socket.id,
+            msg
+        });
+    });
+
+    socket.on("disconnect", () => {
+        delete players[socket.id];
+        io.emit("remove", socket.id);
+    });
 
 });
 
-socket.on("move",(data)=>{
-players[socket.id] = data;
-socket.broadcast.emit("update",{id:socket.id,data});
-});
+/* =========================
+   START SERVER
+========================= */
 
-socket.on("chat",(msg)=>{
-io.emit("chat",{id:socket.id,msg});
-});
+const PORT = process.env.PORT || 3000;
 
-socket.on("disconnect",()=>{
-delete players[socket.id];
-io.emit("remove",socket.id);
-});
-
-});
-
-http.listen(process.env.PORT || 3000,()=>{
-console.log("NovaBlocks v3.7 running");
+http.listen(PORT, () => {
+    console.log("NovaBlocks running on port " + PORT);
 });
